4ff54d4e57d3afddb84e645722fe666d032d8720
Ted Ding <tedyiningding@gmail.com>
2023-10-27 09:30:43 +0100

    fix bug in the inconsistency in the order of hori/vert gradients between the gradient/negative divergence operators and the anisotrotic diffusion tensor computation

--- util/operators/operator_D.m
+++ util/operators/operator_D.m
@@ -3,8 +3,8 @@ function D_x = operator_D(x)
 % a mapping from (H-by-W) to (H-by-W-by-2)
 try
     D_x = cat(3, ...
-              [diff(x, 1, 1); zeros(1, size(x, 2))], ... % vertical gradient
-              [diff(x, 1, 2), zeros(size(x, 1), 1)]);    % horizontal gradient
+              [diff(x, 1, 2), zeros(size(x, 1), 1)], ...    % horizontal gradient
+              [diff(x, 1, 1); zeros(1, size(x, 2))]);       % vertical gradient
 catch
     warning('t')
 end
--- util/operators/operator_Dadj.m
+++ util/operators/operator_Dadj.m
@@ -1,6 +1,9 @@
 function Dadj_u = operator_Dadj(u)
-% applies the negative diverence operator (i.e. the adjoint of the gradient operator) with Dirichlet boundary condition
+% applies the negative divegence operator (i.e. the adjoint of the gradient operator) with Dirichlet boundary condition
 % a mapping from (H-by-W-by-2) to (H-by-W)
-    Dadj_u = [-u(1, :, 1); -diff(u(1:end-1, :, 1), 1, 1); u(end-1, :, 1)] ...
-           + [-u(:, 1, 2), -diff(u(:, 1:end-1, 2), 1, 2), u(:, end-1, 2)];
+
+    h = -[u(:, 1, 1), diff(u(:, 1:end-1, 1), 1, 2), -u(:, end-1, 1)];
+    v = -[u(1, :, 2); diff(u(1:end-1, :, 2), 1, 1); -u(end-1, :, 2)];
+
+    Dadj_u = h + v;
 end
